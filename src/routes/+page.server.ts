38b045ed6138edbe1a936e7a15a6a1c2d93c71a6
sandikodev <androxoss@hotmail.com>
2024-02-20 14:48:01 +0700

    binding action

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,6 @@
+import type { Actions } from '@sveltejs/kit';
 import { RouterOSAPI } from 'node-routeros';
+
 
 function customSort(a, b) {
     const nameA = a['host-name'].match(/([A-Z]+)-(\w+)/);
@@ -21,7 +23,7 @@ export async function load() {
     try {
         await conn.connect();
         const hostQuery = await conn.write('/ip/dhcp-server/lease/print');
-        console.log(hostQuery)
+        // console.log(hostQuery)
         // use this snippet for filtering specific prefix
         // filter(obj => obj['host-name'].startsWith('PC-A'))
         const hostData = hostQuery.map(obj => ({
@@ -32,13 +34,19 @@ export async function load() {
             'status': obj['status'] as string
         })).sort(customSort);
 
-        const userQuery = await conn.write('/ip/hotspot/active/print');
-        // console.log(userQuery);
+        const userActive = await conn.write('/ip/hotspot/active/print');
+        const userHost = await conn.write('/ip/hotspot/host/print');
+        const userBinding = await conn.write('/ip/hotspot/ip-binding/print');
+        // console.log(userHost.some(obj => obj['mac-address'] == '08:00:27:4A:24:E4'));
 
         conn.close();
         return {
             komputer: hostData,
-            user: userQuery
+            user: {
+                active: userActive,
+                host: userHost,
+                binding: userBinding
+            }
         };
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -49,3 +57,83 @@ export async function load() {
         };
     }
 }
+
+export const actions: Actions = {
+    binding: async (event) => {
+        const conn = new RouterOSAPI({
+            host: '172.16.1.23',
+            user: 'admin',
+            password: '',
+        });
+
+        await conn.connect();
+        const data = await event.request.formData();
+
+        const bindings = await conn.write('/ip/hotspot/ip-binding/print')
+        let result, id = bindings.filter(obj => obj['mac-address'] == data.get('target'))[0]['.id']
+
+        if (id) {
+            result = await conn.write('/ip/hotspot/ip-binding/disable', [
+                `=.id=${id}`,
+            ])
+        }
+        
+        if (!id) {
+            const hosts = await conn.write('/ip/hotspot/host/print')
+            id = hosts.filter(obj => obj['mac-address'] == data.get('target'))[0]['.id']
+
+            result = await conn.write('/ip/hotspot/host/make-binding', [
+                `=.id=${id}`
+            ])
+        }
+
+
+        conn.close();
+        return { success: true, data: result };
+    },
+    unBinding: async (event) => {
+        const conn = new RouterOSAPI({
+            host: '172.16.1.23',
+            user: 'admin',
+            password: '',
+        });
+        await conn.connect();
+        const data = await event.request.formData();
+
+        const bindings = await conn.write('/ip/hotspot/ip-binding/print')
+        let id = bindings.filter(obj => obj['mac-address'] == data.get('target'))[0]['.id']
+
+        let result = await conn.write('/ip/hotspot/ip-binding/disable', [
+            `=.id=${id}`,
+        ])
+
+        conn.close();
+        return { success: true, data: result };
+    },
+
+    bindBypass: async (event) => {
+        const conn = new RouterOSAPI({
+            host: '172.16.1.23',
+            user: 'admin',
+            password: '',
+        });
+
+        await conn.connect();
+        const data = await event.request.formData();
+        // Object.fromEntries(data)
+        // ip hotspot ip-binding set [find where address=10.10.10.99] type=regular 
+
+        const bindings = await conn.write('/ip/hotspot/ip-binding/print');
+        const id = bindings.filter(obj => obj['mac-address'] == data.get('target'))[0]['.id']
+
+        const Set = await conn.write('/ip/hotspot/ip-binding/set', [
+            `?.id=0`,
+            '=type=regular'
+        ]);
+
+        // console.log(id);
+
+        conn.close();
+        return { success: true };
+    },
+};
