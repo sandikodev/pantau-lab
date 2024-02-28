61c8bd9f7929db2e96d5707f8c07739cb538f49d
sandikodev <androxoss@hotmail.com>
2024-02-28 12:10:29 +0700

    data

---
+++ src/lib/data.ts
@@ -0,0 +1,274 @@
+export const data = [
+    {
+        "host-name": "L3-C7",
+        "active-mac-address": "10:E7:C6:4E:31:1E",
+        "active-address": "192.168.0.104",
+        "address": "192.168.0.104",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-D2",
+        "active-mac-address": "10:E7:C6:4E:2D:24",
+        "active-address": "192.168.0.97",
+        "address": "192.168.0.97",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-C2",
+        "active-mac-address": "10:E7:C6:4E:11:1B",
+        "active-address": "192.168.0.100",
+        "address": "192.168.0.100",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "SERVER-LAB3",
+        "active-mac-address": "50:3E:AA:12:F9:BD",
+        "active-address": "192.168.0.126",
+        "address": "192.168.0.126",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-A3",
+        "active-mac-address": "10:E7:C6:4E:30:81",
+        "active-address": "192.168.0.122",
+        "address": "192.168.0.122",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-A7",
+        "active-mac-address": "98:EE:CB:75:97:64",
+        "active-address": "192.168.0.117",
+        "address": "192.168.0.117",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-A5",
+        "active-mac-address": "10:E7:C6:4E:2C:CB",
+        "active-address": "192.168.0.120",
+        "address": "192.168.0.120",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-B5",
+        "active-mac-address": "98:EE:CB:5B:46:4B",
+        "active-address": "192.168.0.109",
+        "address": "192.168.0.109",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-A8",
+        "active-mac-address": "98:EE:CB:75:97:16",
+        "active-address": "192.168.0.116",
+        "address": "192.168.0.116",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-A6",
+        "active-mac-address": "98:EE:CB:75:48:6A",
+        "active-address": "192.168.0.119",
+        "address": "192.168.0.119",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-D4",
+        "active-mac-address": "10:E7:C6:4E:30:7E",
+        "active-address": "192.168.0.95",
+        "address": "192.168.0.95",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-D3",
+        "active-mac-address": "10:E7:C6:4D:FC:DE",
+        "active-address": "192.168.0.96",
+        "address": "192.168.0.96",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-A2",
+        "active-mac-address": "10:E7:C6:4E:31:5F",
+        "active-address": "192.168.0.123",
+        "address": "192.168.0.123",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-C1",
+        "active-mac-address": "10:E7:C6:4E:0C:5C",
+        "active-address": "192.168.0.125",
+        "address": "192.168.0.125",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-A1",
+        "active-mac-address": "10:E7:C6:4E:30:F2",
+        "active-address": "192.168.0.124",
+        "address": "192.168.0.124",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-C9",
+        "active-mac-address": "10:E7:C6:4E:31:AC",
+        "active-address": "192.168.0.106",
+        "address": "192.168.0.106",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-D7",
+        "active-mac-address": "10:E7:C6:4E:0C:8B",
+        "active-address": "192.168.0.92",
+        "address": "192.168.0.92",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-C8",
+        "active-mac-address": "10:E7:C6:4E:31:95",
+        "active-address": "192.168.0.105",
+        "address": "192.168.0.105",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-C6",
+        "active-mac-address": "10:E7:C6:4E:0F:76",
+        "active-address": "192.168.0.103",
+        "address": "192.168.0.103",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-B6",
+        "active-mac-address": "98:EE:CB:5B:46:97",
+        "active-address": "192.168.0.110",
+        "address": "192.168.0.110",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-B8",
+        "active-mac-address": "98:EE:CB:5B:38:85",
+        "active-address": "192.168.0.108",
+        "address": "192.168.0.108",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-B1",
+        "active-mac-address": "98:EE:CB:5B:38:A9",
+        "active-address": "192.168.0.114",
+        "address": "192.168.0.114",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-C5",
+        "active-mac-address": "10:E7:C6:4E:30:DB",
+        "active-address": "192.168.0.102",
+        "address": "192.168.0.102",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-C4",
+        "active-mac-address": "10:E7:C6:4E:19:87",
+        "active-address": "192.168.0.101",
+        "address": "192.168.0.101",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-C3",
+        "active-mac-address": "10:E7:C6:4E:2F:B9",
+        "active-address": "192.168.0.99",
+        "address": "192.168.0.99",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-B2",
+        "active-mac-address": "98:EE:CB:5B:39:01",
+        "active-address": "192.168.0.113",
+        "address": "192.168.0.113",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-B4",
+        "active-mac-address": "98:EE:CB:5B:45:12",
+        "active-address": "192.168.0.111",
+        "address": "192.168.0.111",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-B3",
+        "active-mac-address": "98:EE:CB:5B:46:B5",
+        "active-address": "192.168.0.112",
+        "address": "192.168.0.112",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-D5",
+        "active-mac-address": "10:E7:C6:4E:11:38",
+        "active-address": "192.168.0.94",
+        "address": "192.168.0.94",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-D1",
+        "active-mac-address": "10:E7:C6:4E:12:5C",
+        "active-address": "192.168.0.98",
+        "address": "192.168.0.98",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-D9",
+        "active-mac-address": "10:E7:C6:4E:0B:8B",
+        "active-address": "192.168.0.91",
+        "address": "192.168.0.91",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-D8",
+        "active-mac-address": "10:E7:C6:4E:0A:5F",
+        "active-address": "192.168.0.107",
+        "address": "192.168.0.107",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-D6",
+        "active-mac-address": "10:E7:C6:4E:2C:0D",
+        "active-address": "192.168.0.93",
+        "address": "192.168.0.93",
+        "dynamic": "true",
+        "status": "bound"
+    },
+    {
+        "host-name": "L3-A4",
+        "active-mac-address": "98:EE:CB:04:03:55",
+        "active-address": "192.168.0.121",
+        "address": "192.168.0.121",
+        "dynamic": "true",
+        "status": "bound"
+    }
+]
